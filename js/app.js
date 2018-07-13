4020b54e6f4d8b2a204ee69128195b01ae4a5279
Mary Francis <maryfsc@disroot.org>
2018-07-13 20:07:09 -0300

    Testando menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
-/*
- * Funcionalidad de tu producto
- */
+var mainContent = document.getElementById('main-content');
+
+var menuList = document.getElementsByClassName('menu-list');
+
+for li in menuList {
+	menuList[li].addEventListener('click', showMenu);
+}
+
+
+function showMenu {
+	var listButton = document.getElementsByClassName('list-button');
+
+	if (menuList.classList.contains('hidden')){
+		menuList.classList.remove('hidden');
+		menuList.classList.add('show');
+	}
+}
+
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
