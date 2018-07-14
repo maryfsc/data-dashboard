d86fd5633704f3c7ef5bfef5e438f2aa1134ca50
JulianaAugusta <juli_aug@hotmail.com>
2018-07-14 19:31:50 -0300

    Teste nova branch

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var mainContent = document.getElementById('main-content'); // ainda não utilizada
 
-// pegando o elemento HTML do botão de menu e acrescentando o evento de click 
+// pegando o elemento HTML do botão de menu e acrescentando o evento de click
 var menuButton = document.getElementById('menu-button');
 menuButton.addEventListener('click', showMenu);
 
@@ -20,9 +20,24 @@ function showMenu() {
      listButton[i].classList.add('hidden');
    }
   }
-  
-	
+
+
 }
+var studentsList = document.getElementById('drop-menu');
+dropMenu.addEventListener('change', carregaProgramadoras);
+
+function carregaProgramadoras(){
+	var sede = dropMenu.value;
+	var listaProgramadoras = document.getElementById('exibe-programadoras');
+	listaProgramadoras.innerHTML = '';
+	for (turma in data[sede]) {
+		for (i in data[sede][turma]['students']) {
+			var img = document.createElement('img');
+			img.src = data[sede][turma]['students'][i]['photo'];
+			listaProgramadoras.appendChild(img);
+		}
+	}
+};
 
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
