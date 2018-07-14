f7a2ecae4baddcc79420692fa70d1a9225d1f673
Mary Francis <maryfsc@disroot.org>
2018-07-14 14:53:17 -0300

    Implementando o menu hamburger.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
-var mainContent = document.getElementById('main-content');
+var mainContent = document.getElementById('main-content'); // ainda não utilizada
 
-var menuList = document.getElementsByClassName('menu-list');
+// pegando o elemento HTML do botão de menu e acrescentando o evento de click 
+var menuButton = document.getElementById('menu-button');
+menuButton.addEventListener('click', showMenu);
 
-for li in menuList {
-	menuList[li].addEventListener('click', showMenu);
-}
+function showMenu() {
 
+  // retorna um array com todos os elementos HTML com classe 'list-button'
+  var listButton = document.getElementsByClassName('list-button');
 
-function showMenu {
-	var listButton = document.getElementsByClassName('list-button');
-
-	if (menuList.classList.contains('hidden')){
-		menuList.classList.remove('hidden');
-		menuList.classList.add('show');
-	}
+  // percorre o array listButton removendo e acrescentando classes
+  // pré-definidas no CSS
+  for (i = 0; i < listButton.length; i++) {
+   if (listButton[i].classList.contains('hidden')){
+     listButton[i].classList.remove('hidden');
+     listButton[i].classList.add('show');
+   } else {
+     listButton[i].classList.remove('show');
+     listButton[i].classList.add('hidden');
+   }
+  }
+  
+	
 }
 
 
